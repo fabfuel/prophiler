332c10a230361953944e27befb9a48be5e450700
Fabian Fuelling <fuelling@orangecloud.de>
2014-11-13 21:53:55 +0100

    Add Fabfuel\Mongo adapter

---
+++ src/Fabfuel/Prophiler/Adapter/Mongo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author @fabfuel <fabian@fabfuel.de>
+ * @created 13.11.14, 20:41 
+ */
+namespace Fabfuel\Prophiler\Adapter;
+
+use Fabfuel\Prophiler\Profiler;
+use Mongo\Profiler\ProfilerInterface;
+
+class MongoProfiler extends Profiler implements ProfilerInterface
+{
+}
