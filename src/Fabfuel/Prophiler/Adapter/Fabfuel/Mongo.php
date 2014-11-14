6bf5db042b705976d6253cb956c2742f47b4062e
Fabian Fuelling <fuelling@orangecloud.de>
2014-11-14 07:11:26 +0100

    Create adapter for Fabfuel\Mongo

---
+++ src/Fabfuel/Prophiler/Adapter/Fabfuel/Mongo.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author @fabfuel <fabian@fabfuel.de>
+ * @created 13.11.14, 20:41 
+ */
+namespace Fabfuel\Prophiler\Adapter\Fabfuel;
+
+use Fabfuel\Prophiler\Adapter\AdapterAbstract;
+use Mongo\Profiler\ProfilerInterface;
+
+/**
+ * Class Mongo
+ * Profiler adapter for Fabfuel\Mongo
+ *
+ * Usage:
+ * $profiler = new \Fabfuel\Prophiler\Profiler();
+ * $adapter = new \Fabfuel\Prophiler\Adapter\Fabfuel\Mongo($profiler);
+ * $mongoDb->setProfiler($adapter);
+ *
+ * @package Fabfuel\Prophiler\Adapter\Fabfuel
+ */
+class Mongo extends AdapterAbstract implements ProfilerInterface
+{
+    /**
+     * Start a new benchmark
+     *
+     * @param string $name Unique identifier like e.g. Class::Method (\Foobar\MyClass::doSomething)
+     * @param array $metadata Addtional interesting metadata for this benchmark
+     * @return string identifier token
+     */
+    public function start($name, array $metadata = [])
+    {
+        $this->getProfiler()->start($name, $metadata);
+    }
+
+    /**
+     * Stop a running benchmark
+     *
+     * @param string $token benchmark identifier
+     * @return void
+     */
+    public function stop($token)
+    {
+        $this->getProfiler()->start($token);
+    }
+}
