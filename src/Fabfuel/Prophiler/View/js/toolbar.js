817891e6b83899472dfa0fed37044db9c55b431b
Marco Troisi <marco.troisi@rocket-internet.de>
2014-11-26 16:02:49 +0100

    Now Javascript is inside a more elegant Object

--- src/Fabfuel/Prophiler/View/js/toolbar.js
+++ src/Fabfuel/Prophiler/View/js/toolbar.js
@@ -1,3 +1,31 @@
 /**
- * Created by marco on 26.11.14.
+ * @author @marcotroisi <hello@marcotroisi.com>
+ * @created 26.11.14
  */
+
+
+var Prophiler = Prophiler || {};
+(function(Prophiler) {
+
+    Prophiler.slideToggleData = function(name) {
+        var element = document.getElementById(name);
+        if (element.style.display != 'inherit') {
+            element.style.display = 'inherit';
+            element.style.opacity = '1.0';
+        } else {
+            element.style.opacity = '0';
+            element.style.display = 'none';
+        }
+    };
+
+    window.addEventListener("load", function () {
+
+        var jsonTags = document.getElementsByTagName('json');
+
+        for (var index = 0; index < jsonTags.length; index++) {
+            var el = jsonTags[index];
+            jsonTags[index].innerHTML = JSON.stringify(JSON.parse(el.innerHTML), null, '    ');
+        }
+    });
+
+})(Prophiler);
