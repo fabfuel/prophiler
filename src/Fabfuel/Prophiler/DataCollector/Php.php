f5c2319fe23a0cc09a5f55d97bb1ee89dfce025a
Marco Troisi <marco.troisi@rocket-internet.de>
2014-11-28 17:17:41 +0100

    phpinfo() added + unit tests updated

--- src/Fabfuel/Prophiler/DataCollector/Php.php
+++ src/Fabfuel/Prophiler/DataCollector/Php.php
@@ -40,9 +40,38 @@ class Php implements DataCollectorInterface
             'PHP version' => phpversion(),
             'Phalcon' => extension_loaded('phalcon'),
             'APC' => extension_loaded('apc'),
+            'PHP info' => $this->phpInfo_array(),
             'Extensions Loaded' => get_loaded_extensions(),
         ];
 
         return $data;
+    }
+
+    /**
+     * Returns the phpinfo() values as an array
+     *
+     * @return array
+     */
+    protected function phpInfo_array()
+    {
+        ob_start();
+        phpinfo();
+        $info_arr = array();
+        $info_lines = explode("\n", strip_tags(ob_get_clean(), "<tr><td><h2>"));
+        $cat = "General";
+        foreach($info_lines as $line)
+        {
+            // new cat?
+            preg_match("~<h2>(.*)</h2>~", $line, $title) ? $cat = $title[1] : null;
+            if(preg_match("~<tr><td[^>]+>([^<]*)</td><td[^>]+>([^<]*)</td></tr>~", $line, $val))
+            {
+                $info_arr[$cat][$val[1]] = $val[2];
+            }
+            elseif(preg_match("~<tr><td[^>]+>([^<]*)</td><td[^>]+>([^<]*)</td><td[^>]+>([^<]*)</td></tr>~", $line, $val))
+            {
+                $info_arr[$cat][$val[1]] = array("local" => $val[2], "master" => $val[3]);
+            }
+        }
+        return $info_arr;
     }
 }
--- tests/Fabfuel/Prophiler/DataCollector/PhpTest.php
+++ tests/Fabfuel/Prophiler/DataCollector/PhpTest.php
@@ -36,6 +36,7 @@ class PhpTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers Fabfuel\Prophiler\DataCollector\Php::getData
+     * @covers Fabfuel\Prophiler\DataCollector\Php::phpInfo_array
      */
     public function testGetData()
     {
@@ -43,9 +44,33 @@ class PhpTest extends \PHPUnit_Framework_TestCase
             'PHP version' => phpversion(),
             'Phalcon' => extension_loaded('phalcon'),
             'APC' => extension_loaded('apc'),
+            'PHP info' => $this->phpInfo_array(),
             'Extensions Loaded' => get_loaded_extensions(),
         ];
 
         $this->assertSame($data, $this->php->getData());
+    }
+
+    protected function phpInfo_array()
+    {
+        ob_start();
+        phpinfo();
+        $info_arr = array();
+        $info_lines = explode("\n", strip_tags(ob_get_clean(), "<tr><td><h2>"));
+        $cat = "General";
+        foreach($info_lines as $line)
+        {
+            // new cat?
+            preg_match("~<h2>(.*)</h2>~", $line, $title) ? $cat = $title[1] : null;
+            if(preg_match("~<tr><td[^>]+>([^<]*)</td><td[^>]+>([^<]*)</td></tr>~", $line, $val))
+            {
+                $info_arr[$cat][$val[1]] = $val[2];
+            }
+            elseif(preg_match("~<tr><td[^>]+>([^<]*)</td><td[^>]+>([^<]*)</td><td[^>]+>([^<]*)</td></tr>~", $line, $val))
+            {
+                $info_arr[$cat][$val[1]] = array("local" => $val[2], "master" => $val[3]);
+            }
+        }
+        return $info_arr;
     }
 }
