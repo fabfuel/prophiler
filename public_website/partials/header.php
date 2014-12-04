bbe05e23101df39026dbd5b0181a5de18e704f1c
Marco Troisi <marco.troisi@rocket-internet.de>
2014-12-04 14:38:38 +0100

    #time 1h A new public website for the Prophiler Toolbar

---
+++ public_website/partials/header.php
@@ -0,0 +1,39 @@
+<!-- Header -->
+<div id="header-wrapper">
+    <div id="header" class="container">
+
+        <!-- Logo -->
+        <h1 id="logo"><a href="index.html">Prophiler Toolbar</a></h1>
+
+        <!-- Nav -->
+        <nav id="nav">
+            <ul>
+                <li><a href="how-it-works.php">How it works</a></li>
+                <li><a target="_blank" href="http://prophiler.fabfuel.de">Try a Demo</a></li>
+                <li class="break"><a href="https://github.com/fabfuel/prophiler">Fork me on Github</a></li>
+                <li><a href="index.php#contacts">Contacts</a></li>
+            </ul>
+        </nav>
+
+    </div>
+
+    <!-- Hero -->
+    <section id="hero" class="container">
+        <header>
+            <h2>
+                Prophiler
+                <br />
+                A Phalcon Profiler & Dev Toolbar
+            </h2>
+        </header>
+        <p>
+            Designed and built by <a href="http://github.com/fabfuel">Fabfuel</a>.
+            <br />
+            Released for free under
+            the <a href="https://raw.githubusercontent.com/fabfuel/prophiler/develop/LICENSE.md">MIT license</a>.</p>
+        <ul class="actions">
+            <li><a href="how-it-works.php" class="button">See how it works</a></li>
+        </ul>
+    </section>
+
+</div>
