f188f0330c7f112d166cf666f3ad494c646ec15c
Fabian Fuelling <fuelling@orangecloud.de>
2014-12-05 22:16:20 +0100

    Reactivate skew-layers in demo and make toolbar a bit more responsive

--- public/partials/head.php
+++ public/partials/head.php
@@ -7,6 +7,7 @@
     <script src="/js/jquery.min.js"></script>
     <script src="/js/jquery.dropotron.min.js"></script>
     <script src="/js/skel.min.js"></script>
+    <script src="/js/skel-layers.min.js"></script>
     <script src="/js/init.js"></script>
     <noscript>
         <link rel="stylesheet" href="/css/skel.css" />
