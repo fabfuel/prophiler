41799e19ed7d8df1670dcb8c67da05f759427ef4
Fabian Fülling <github@fabfuel.de>
2014-12-09 10:50:00 +0100

    Fix typo on demo page

--- public/demo.php
+++ public/demo.php
@@ -26,7 +26,7 @@
                         The server worked hard to build this page...<br>
                         Okay, that's fake.<br>
                         But hey, this is just a demo.<br>
-                        To see the Phophiler in action, just click one of the buttons above.<br>
+                        To see the Prophiler in action, just click one of the buttons above.<br>
                         And now some placeholder text.
                     </p>
                     <p>Lorem ipsum usu amet dicat nullam ea. Nec detracto lucilius democritum in, ne usu delenit offendit deterruisset. Recusabo iracundia molestiae ea pro, suas dicta nemore an cum, erat dolorum nonummy mel ea. Iisque labores liberavisse in mei, dico laoreet elaboraret nam et, iudico verterem platonem est an. Te usu paulo vidisse epicuri, facilis mentitum liberavisse vel ut, movet iriure invidunt ut quo. Ad melius mnesarchum scribentur eum, mel at mundi impetus utroque.</p>
