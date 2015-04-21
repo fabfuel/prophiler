f5f051af086501d52c7bdccba4ebbcec835aeac6
Fabian Fuelling <fabian.fuelling@rocket-internet.de>
2015-04-21 18:04:25 +0200

    Show alerts (logs with severity "error", "emergency", "critical" or "alert") marked red in toolbar

--- src/Fabfuel/Prophiler/Iterator/ComponentFilteredIterator.php
+++ src/Fabfuel/Prophiler/Iterator/ComponentFilteredIterator.php
@@ -5,8 +5,14 @@
  */
 namespace Fabfuel\Prophiler\Iterator;
 
+use Fabfuel\Prophiler\Benchmark\BenchmarkInterface;
 use Fabfuel\Prophiler\ProfilerInterface;
 
+/**
+ * Class ComponentFilteredIterator
+ * @package Fabfuel\Prophiler\Iterator
+ * @method BenchmarkInterface current
+ */
 class ComponentFilteredIterator extends \FilterIterator implements \Countable
 {
     /**
@@ -15,11 +21,18 @@ class ComponentFilteredIterator extends \FilterIterator implements \Countable
     protected $component;
 
     /**
+     * @var array
+     */
+    protected $filters = [];
+
+    /**
      * @param ProfilerInterface|\Iterator $profiler
      * @param string $component
+     * @param array $filters Additional filters (e.g. ['severity' => ['alert', 'emergency', 'error']
      */
-    public function __construct(ProfilerInterface $profiler, $component)
+    public function __construct(ProfilerInterface $profiler, $component, array $filters = [])
     {
+        $this->filters = $filters;
         $this->component = $component;
         parent::__construct($profiler);
     }
@@ -31,7 +44,26 @@ class ComponentFilteredIterator extends \FilterIterator implements \Countable
      */
     public function accept()
     {
-        return $this->current()->getComponent() === $this->component;
+        if ($this->current()->getComponent() !== $this->component) {
+            return false;
+        }
+        return $this->acceptFilters();
+    }
+
+    /**
+     * @return bool
+     */
+    protected function acceptFilters()
+    {
+        foreach ($this->filters as $field => $value) {
+            if (is_array($value) && !in_array($this->current()->getMetadataValue($field), $value, true)) {
+                return false;
+            } elseif (!is_array($value) && $this->current()->getMetadataValue($field) !== $value) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
